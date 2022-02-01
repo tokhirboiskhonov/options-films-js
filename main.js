dd5bf1f864dab55030fcd677c256b474e2b3298e
tokhirboiskhonov <tokhir060@gmail.com>
2022-02-01 08:31:57 +0500

    Add option JS

---
+++ main.js
@@ -0,0 +1,72 @@
+const elFilmsList = document.querySelector('.films__list');
+const elFilmTemplate = document.querySelector('#film-template').content;
+const elForm = document.querySelector('.form');
+const elFormSelect = document.querySelector('.form__select');
+const elFormBtn = document.querySelector('.form__btn')
+
+elForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let select = elFormSelect.value;
+
+    let newarray = [];
+
+    films.forEach(film => {
+        if(select === "All"){
+            newarray.push(film);
+        }
+        if(film.genres.includes(select)){
+            newarray.push(film);
+        }
+    })
+
+    renderFilms(newarray, elFilmsList);
+
+
+})
+
+let genresArr = ['All', 'Action', 'Comedy', 'Fantasy', 'Adventure', 'Science Fiction', 'Thriller', 'Horror', 'Animation', 'Family', 'Documentary', 'Mystery', 'Drama', 'TV Movie', 'Music', 'Crime', 'History', 'Romance'];
+
+function renderOption (genresArr, element) {
+    genresArr.forEach(genresFilms => {
+        let newOption = document.createElement('option');
+        newOption.setAttribute('value', genresFilms);
+        newOption.textContent = genresFilms;
+        elFormSelect.appendChild(newOption);
+    })
+}
+
+renderOption(genresArr, elFormSelect)
+
+function normalizeDate (dateFormat) {
+
+    let date = new Date (dateFormat);
+    let day = String(date.getDate()).padStart(2, 0);
+    let month = String(date.getMonth() + 1).padStart(2, 0);
+    let year = String(date.getFullYear()).padStart(2, 0);
+
+    return (day + '.' + month + '.' + year);
+}
+
+let renderFilms = (array,element)=>{
+    elFilmsList.innerHTML = null;
+
+    array.forEach(film => {
+
+        let filmsTemplate = elFilmTemplate.cloneNode(true);
+        
+        filmsTemplate.querySelector('.films__title').textContent = film.title;
+        
+        filmsTemplate.querySelector('.films__img').src = film.poster;
+        
+        filmsTemplate.querySelector('.films__overview').textContent = film.overview;
+        
+        filmsTemplate.querySelector(".films__time").textContent = normalizeDate(film.release_date);
+
+        filmsTemplate.querySelector('.films__genre-text').textContent = film.genres;
+        
+        element.appendChild(filmsTemplate);
+    });
+};
+
+renderFilms(films, elFilmsList)
